b9ee317131fdb161d767f126e0bacb03ff179b62
User <user@example.com>
2026-06-16 13:04:57 +0700

    Fix 404 routing by switching to HashRouter and update favicon links

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <ThemeProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </ThemeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
